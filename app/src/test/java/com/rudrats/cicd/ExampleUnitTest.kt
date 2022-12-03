f1e63dad1d0cdd81a0aab9a9c3b9e990cabe4f1a
Abhishek Kr Rai <abhishekraics001@gmail.com>
2022-12-03 21:05:50 +0530

    Add Unit test cases

--- app/src/test/java/com/rudrats/cicd/ExampleUnitTest.kt
+++ app/src/test/java/com/rudrats/cicd/ExampleUnitTest.kt
@@ -10,8 +10,24 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun addition_isTrue() {
+        assertEquals("Abhishek", "Abhi + Rai")
+    }
+
+    @Test
+    fun addition_isTrueOrFalse() {
+        assertTrue(true);
+    }
+
+    @Test
+    fun addition_isTrueOrFalse2() {
+        assertTrue(false);
     }
 }
